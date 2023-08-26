e9a731df1462b0b890453ed2bb9d9682e27359b8
renegadelhaedu <renegadelha@gmail.com>
2023-08-26 13:19:24 -0300

    initial launch

---
+++ code6.py
@@ -0,0 +1,35 @@
+#tipos de variáveis str int float
+#nome = 'rene nobrega de sousa gadelha'
+
+#operador de atribuição =
+#numero = 'CZ'
+#numero2 = numero * 4
+
+#funções de entrada e saída
+#nome_usuario = input('digite seu nome')
+#print('meu nome e ', nome_usuario)
+
+#operadores aritmeticos + - * /
+soma = 2.6 + 3
+#print(type(soma))
+sub = 7.5 - 4.5
+#print(type(sub))
+div = 6 / 6
+#print(type(div))
+mult = 2 * 7.
+#print(type(mult))
+
+#concatenação de strings
+#print('2' + '7')
+
+
+#operador aritmetico de exponenciação/potenciação **
+#resposta = 2 ** 9
+#print(resposta)
+
+#operador aritmetico de divisão inteira //
+print(10//3)
+
+#operador aritmetico do resto da divisao inteira
+print(5 % 2)
+
