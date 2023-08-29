15cb9bdd9fd46db96a93c8053c34419cc21e3b51
renegadelhaedu <renegadelha@gmail.com>
2023-08-28 21:52:40 -0300

    aula28-08

---
+++ code10.py
@@ -0,0 +1,9 @@
+bruto = float(input('digite seu salario bruto'))
+
+if(bruto < 2300):
+    liquido = bruto * 0.9
+else:
+    liquido = bruto * 0.6
+
+print(liquido)
+
---
+++ exeDec3.py
@@ -0,0 +1,7 @@
+sexo = 'M'
+
+if(sexo == 'F'):
+    print('o sexo escolhido foi o feminino')
+
+if(sexo == 'M'):
+    print('o sexo escolhido foi o masculino')
